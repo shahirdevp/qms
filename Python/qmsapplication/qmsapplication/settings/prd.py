cf083ffc89e5739dc6a0aa3a01485175b01aeb2b
shahirdevp <shahirkm@5ine.in>
2019-04-23 17:47:09 +0530

    production

--- Python/qmsapplication/qmsapplication/settings/prd.py
+++ Python/qmsapplication/qmsapplication/settings/prd.py
@@ -5,4 +5,4 @@ DEBUG = False
 # SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: update this when you have the production host
-ALLOWED_HOSTS = ['http://dreamtravel.in/']
+ALLOWED_HOSTS = ['http://dreamtravel.in/', '13.126.142.73']
